e7d7c56837889b695589a712b14bef81008859dc
Emmanuel Obonyo <emmyobonyo@gmail.com>
2022-09-28 11:06:59 +0300

    some stuff were ADDED

---
+++ NodeAPI/index.js
@@ -0,0 +1,33 @@
+const fs = require("fs").promises
+const express = require("express")
+const cors = require("cors")
+const _ = require("lodash")
+const { v4:uuid } = require("uuid")
+
+//Set up the API server
+
+const app = express()
+app.use(express.json())
+
+app.get("/outfit", (req, res) => {
+    const tops = ["Black", "Blue", "Navy", "Yellow"]
+    const jeans = ["Green", "Cyam", "Violet", "Rainbow"]
+    const shoes = ["Cyan", "Gray", "Sky blue", "Black"]
+    res.json({
+        top: _.sample(tops),
+        jeans: _.sample(jeans),
+        shoes: _.sample(shoes)
+    })
+} )
+
+app.post("/comments", (req, res) => {
+    const id = uuid();
+    const comment = req.body.comment
+    if(!content) {
+        return res.sendStatus(400);
+    }
+    // console.log(comment)
+    res.sendStatus(201)
+})
+
+app.listen(3000, () => console.log('The server is running....'));
---
+++ NodeAPI/learning.js
@@ -0,0 +1,32 @@
+// Javascript Data types and Data structures.
+
+let foo = 42 // foo is now a number.
+foo = 'bar'
+foo = true
+
+// Javascript allows implicit type comversion
+
+const fool = 42 // foo in now a number
+const result = foo + '1' // Javascript coerces foo to be a string, so it can concatenated with the other operand
+console.log(result) // 421 but now 421 is a string.
+
+//Primitive types -- Represented at the lowest level of the language
+
+//null is a falsy value
+
+//BigInt --> Allows us to work with numbers greater than the max safe integer
+// Definitely write about BigInt (mostly for my won learning)
+const x = 1n
+console.log(x + BigInt(Number.MAX_SAFE_INTEGER))
+
+// We cn use operators with BigInt
+
+// NaN is not equal to itself.
+// Nan equates to false
+console.log(!!NaN)
+
+//The first element of a string is at index 0. The length of a string is the number of elements in it.
+
+//Javascript strings are immutable Once it's created, it can not be changed. 
+
+//Stopped at Javascript Stings
